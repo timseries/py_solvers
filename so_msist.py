453894d4e6e336f4a37919fe3f48ef236b112f8f
timseries <timothy.daniel.roberts@gmail.com>
2014-02-21 00:29:37 +0000

    added hmt methods to msist, need to generalize for multiple dimensions...

--- so_msist.py
+++ so_msist.py
@@ -8,6 +8,8 @@ from py_solvers.solver import Solver
 from py_operators.operator import Operator
 from py_operators.operator_comp import OperatorComp
 
+import scipy as ss
+
 class MSIST(Solver):
     """
     Solver which performs the MSIST algorithm, with several different variants.
@@ -25,6 +27,7 @@ class MSIST(Solver):
         self.W = self.W.ls_operators[0] #assume we just have one transform
         self.alpha = self.get_val('alpha',True)
         self.str_group_structure = self.get_val('grouptypes',False)
+        
 
     def solve(self,dict_in):
         """
@@ -63,14 +66,19 @@ class MSIST(Solver):
             epsilon,nu = self.get_epsilon_nu()
             dict_in['nu_sq'] = nu**2
             dict_in['epsilon_sq'] = epsilon**2
-        elif self.str_sparse_pen == 'vbmm': #vbmm    
+        elif (self.str_sparse_pen == 'vbmm' or  #vbmm    
+              self.str_sparse_pen == 'vbmm_hmt'):
             nu = self.get_val('nustart',True) * np.ones(self.int_iterations,)
             dict_in['nu_sq'] = nu**2
-            p_a = self.get_val('p_a',True)
-            p_b_0 = self.get_val('p_b_0',True)
-            p_k = self.get_val('p_k',True)
-            p_theta = self.get_val('p_theta',True)
-            b_n = WS(np.zeros(w_n.ary_lowpass.shape),(w_n.one_subband(0)).tup_coeffs)
+            if self.str_sparse_pen == 'vbmm':
+                p_a = self.get_val('p_a',True)
+                p_b_0 = self.get_val('p_b_0',True)
+                p_k = self.get_val('p_k',True)
+                p_theta = self.get_val('p_theta',True)
+                b_n = WS(np.zeros(w_n.ary_lowpass.shape),
+                         (w_n.one_subband(0)).tup_coeffs)
+            else: #hmt variant
+                ary_a = get_gamma_shapes(W * dict_in['x_0'])
             for s in arange(w_n.int_subbands):
                 b_n.set_subband(s, p_b_0)
         else:
@@ -80,17 +88,32 @@ class MSIST(Solver):
         for n in np.arange(self.int_iterations):
             f_resid = ifftn(y_hat - H * x_n)
             w_resid = W * ifftn(~H * f_resid)
-            for s in arange(w_n.int_subbands):
+            for s in xrange(w_n.int_subbands-1,-1,-1):
                 #variance estimate
-                if self.str_solver_variant == 'solvereal': #msist
+                if self.str_sparse_pen == 'l0rl2': #msist
                     S_n.set_subband(s,(1.0 / ((1.0 / g_i) * nabs(w_n.get_subband(s))**2 + epsilon[n]**2)))
-                elif self.str_solver_variant == 'solvevbmm': #vbmm    
+                elif self.str_solver_variant == 'vbmm': #vbmm    
                     if n == 0:
                         sigma_n = 0
                     else:
                         sigma_n = (1.0 / nu[n]**2 * alpha[s] + S_n.get_subband(s))**(-1)
                     S_n.set_subband(s, (g_i + 2.0 * p_a) / (nabs(w_n.get_subband(s))**2 + sigma_n + 2.0 * b_n.get_subband(s)))
                     b_n.set_subband(s, (p_k + p_a) / (S_n.get_subband(s) + p_theta))
+                elif self.str_solver_variant == 'vbmm_hmt': #vbmm    
+                    if n == 0:
+                        sigma_n = 0
+                    else:
+                        sigma_n = (1.0 / nu[n]**2 * alpha[s] + S_n.get_subband(s))**(-1)
+                    if s < w_n.int_subbands - 6:    
+                        s_parent = w_n.get_subband(s+6)
+                        s_parent_us = np.zeros((2*s_parent.shape[0],2*s_parent.shape[1]))
+                        s_parent_us[0::2,0::2]=s_parent
+                        s_parent_us[0::2,1::2]=s_parent
+                        s_parent_us[1::2,0::2]=s_parent
+                        s_parent_us[1::2,1::2]=s_parent
+                        adj_factor = .5 * .25 * .25
+                    S_n.set_subband(s, (g_i + 2.0 * ary_a[s]) / 
+                                    (nabs(w_n.get_subband(s))**2 + sigma_n + 2.0 * adj_factor * np.abs(s_parent_us)**2))
                 else:
                     raise Exception('no such solver variant')
                 #update current solution
@@ -131,6 +154,20 @@ class MSIST(Solver):
         else:
             raise Exception('no such continuation parameter rule')
         return epsilon,nu
+
+    def get_gamma_shapes(self, ws_coeffs):
+        """Use maximum likelihood to obtain the shape parameters 
+        for each a gamma distribution.
+
+        Return:
+          ary_a (ndarray): a 1-d array of shape parameters
+        """
+        ary_a = np.zeros(ws_coeffs.int_subbands,)
+        for s in xrange(ws_coeffs.int_subbands):
+            subband = ws_coeffs.get_subband(s).flatten()
+            subband_ri = np.concatenate(subband.real,subband.imag)
+            fit_nu, fit_loc, fit_scale = ss.t.fit(subband_ri)
+            ary_a[s] = fit_nu / 2
             
     class Factory:
         def create(self,ps_parameters,str_section):
