557f741c72546d1de147409c2fc186a7a3244687
timseries <timothy.daniel.roberts@gmail.com>
2014-03-09 16:05:00 +0000

    same performance using mad estimator for the noise...

--- so_msist.py
+++ so_msist.py
@@ -106,12 +106,12 @@ class MSIST(Solver):
 
             if self.str_sparse_pen == 'l0rl2_bivar':
                 hh = (nabs(w_n.get_subband(2))).flatten()
-                # sigsq_n = su.mad(hh)/.6745 #eq 7, Sendur BSWLVE paper, why isn't this squared?
+                sigsq_n = su.mad(hh)/.6745 #eq 7, Sendur BSWLVE paper, why isn't this squared?
                 # print sigsq_n
                 # sigsq_n = dict_in['nu_sq'][-1]
                 # sigsq_n = .34809
                 # sigsq_n = .05
-                sigsq_n = self.get_val('nustop', True)**2
+                # sigsq_n = self.get_val('nustop', True)**2
                 sig_n = sqrt(sigsq_n)
                 sqrt3=sqrt(3.0)
             for s in xrange(w_n.int_subbands-1,-1,-1):
