ed1266d3fe710fa66477f6aa0cd0f16f9d1514e5
timseries <timothy.daniel.roberts@gmail.com>
2014-07-04 02:18:01 +0100

    updates...

--- application/compressed_sensing/scripts/generate_mask_boundary.py
+++ application/compressed_sensing/scripts/generate_mask_boundary.py
@@ -4,7 +4,7 @@ import numpy as np
 from skimage.morphology import dilation,disk
 
 #input mask path
-mask_path = '/home/tim/repos/py_solvers/application/data/velocity_imaging/reducing_times_benchmark_spatial_mask.npz'
+mask_path = '/home/tim/repos/py_solvers/application/data/velocity_imaging/2dsine_spatial_mask.npz'
 mask = np.load(mask_path)['arr_0']
 
 disk_struct_el = disk(2)
@@ -21,7 +21,6 @@ plt.imshow(boundary_mask*mask)
 plt.figure()
 plt.imshow(not_boundary_mask)
 
-#save
-np.savez_compressed('/home/tim/repos/py_solvers/application/data/velocity_imaging/reducing_times_benchmark_spatial_mask_boundary.npz',boundary_mask)
+#save boundary mask
+np.savez_compressed('/home/tim/repos/py_solvers/application/data/velocity_imaging/2dsine_spatial_mask_boundary.npz',boundary_mask)
 
-np.savez_compressed('/home/tim/repos/py_solvers/application/data/velocity_imaging/reducing_times_benchmark_spatial_mask_not_boundary.npz',not_boundary_mask)
---
+++ application/compressed_sensing/scripts/synthetic_phase_image.py
@@ -0,0 +1,47 @@
+#script to generate a mask boundary 
+import matplotlib.pyplot as plt
+import numpy as np
+from mpldatacursor import datacursor
+
+#generate a 2-dimensional sinusoid
+periods_x = 3.0
+periods_y = 3.0
+sz_x = 128.0
+sz_y = 128.0
+
+x = np.arange(sz_x)/sz_x*periods_x*2.0*np.pi
+y = np.arange(sz_y)/sz_y*periods_y*2.0*np.pi
+
+x,y = np.mgrid[0.1:.1+2.0 * np.pi * periods_x : 1 / sz_x * periods_x * 2.0 * np.pi, 
+               0.1:.1+2.0 * np.pi * periods_y : 1 / sz_y * periods_y * 2.0 * np.pi]
+
+z = np.sin(x)*np.sin(y)
+# plt.figure()
+# plt.imshow(z)
+# datacursor(display='single')
+
+mask = z.copy()
+mask[mask <= -.5] = False
+mask[mask > 5] = True
+mask = np.array(mask, dtype='bool')
+
+#adjust z to PhaseLowerLimit=-.5, PhaseUpperLimit=6.29
+z += 1
+z /= np.max(np.abs(z))
+z *= 4.9
+z -= -.5
+
+plt.imshow(z * mask)
+
+signal = 1.0 * np.exp(1j * z)
+
+#simple phase unwrapping
+phase = angle(signal)
+phase[phase < -.5] += 2.0 * np.pi
+
+plt.imshow(mask * (phase))
+
+np.savez_compressed('/home/tim/repos/py_solvers/application/data/velocity_imaging/2dsine_fully_sampled_data.npz',signal)
+
+np.savez_compressed('/home/tim/repos/py_solvers/application/data/velocity_imaging/2dsine_spatial_mask.npz',mask)
+
