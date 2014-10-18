7cf8a835da70c64710b3d3f716ab9d0642d701ae
Tim <timothy.daniel.roberts@gmail.com>
2014-10-18 15:45:03 +0100

    update

--- application/main.py
+++ application/main.py
@@ -4,6 +4,7 @@ Python script to prompt the user with a number of applications. Then allow selec
 """
 import os
 import sys
+import numpy as np
 from py_utils.parameter_struct import ParameterStruct
 from py_utils.helpers import convertStr
 from py_utils.section_factory import SectionFactory as sf 
@@ -21,24 +22,30 @@ def main():
     else:    
         ps_path='/home/tim/repos/py_solvers/applications/deconvolution/config/downsampled_cameraman_vbmm.ini'
     ps_params = ParameterStruct(ps_path)
-    if seed_flag!=-1:
-        #set the seed of sec_observe to the commandline arg
-        ps_params.set_key_val_pairs('Observe1', ['seed'], [seed_flag])
-    dict_in = {}
-    sec_input = sf.create_section(ps_params,'Input1')
-    sec_preprocess = sf.create_section(ps_params,'Preprocess1')
-    sec_observe = sf.create_section(ps_params,'Observe1')
-    so_solver = sf.create_section(ps_params,'Solver1')
+    seed_flag = int(seed_flag)
+    if seed_flag!=-1 and seed_flag>0:
+        #sweep the seed and run this a bunch of times
+        seeds = np.arange(0,seed_flag)
+    else:
+        seeds = [None]
+    for seed in seeds:        
+        if len(seeds)>1:
+            ps_params.set_key_val_pairs('Observe1', ['seed'], [seed])
+        dict_in = {}
+        sec_input = sf.create_section(ps_params,'Input1')
+        sec_preprocess = sf.create_section(ps_params,'Preprocess1')
+        sec_observe = sf.create_section(ps_params,'Observe1')
+        so_solver = sf.create_section(ps_params,'Solver1')
 
-    #read, observe, solve, report
-    sec_input.read(dict_in)
-    sec_preprocess.preprocess(dict_in)
-    sec_observe.observe(dict_in)
-    t0 = time.time()    
-    so_solver.solve(dict_in)
-    t1 = time.time()
-    so_solver.results.display()
-    so_solver.results.save()
-    print "Solver finished in " + str(t1-t0) + " seconds"
+        #read, observe, solve, report
+        sec_input.read(dict_in)
+        sec_preprocess.preprocess(dict_in)
+        sec_observe.observe(dict_in)
+        t0 = time.time()    
+        so_solver.solve(dict_in)
+        t1 = time.time()
+        so_solver.results.display()
+        so_solver.results.save()
+        print "Solver finished in " + str(t1-t0) + " seconds"
 if __name__ == "__main__":
     main()
--- run_application.py
+++ run_application.py
@@ -6,11 +6,17 @@ import os
 from py_utils.helpers import convertStr
 from py_utils.parameter_struct import ParameterStruct
 import pdb
+import sys
 
 def main():
     #application selection
     ls_prompts=['application','config']
     str_application_dir = ''
+
+    seed_flag = -1
+    if len(sys.argv)>1:
+        seed_flag = sys.argv[1]
+        print 'seed flag set for sweeping from 0 to ' + str(seed_flag) + '...\n'
     print 'application/configuration selector, use q to break...\n'
     int_sel=''
     str_path=os.path.dirname(os.path.realpath(__file__)) + '/'
@@ -38,7 +44,7 @@ def main():
         ls_file_names = [str_path] #single file execution mode
     for file_name in ls_file_names:
         print 'starting: ' + file_name
-        os.system(ls_prompts[0]+'/main.py ' + file_name)
+        os.system(ls_prompts[0]+'/main.py ' + file_name + ' ' + str(seed_flag))
     
 if __name__ == "__main__":
     main()
